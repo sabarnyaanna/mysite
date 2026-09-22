681dfb7c8dec0fd8b7033e0a97044b85dc44676c
sabarnyaanna <ann.sabarnya@gmail.com>
2026-09-22 15:50:02 +0300

    lab2

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_blog.urls')), # Перенаправляємо головну сторінку до app_blog
+    path('', include('app_blog.urls')), 
 ]
